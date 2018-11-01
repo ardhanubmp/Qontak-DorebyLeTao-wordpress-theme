8d184e5d6b0efb0eb6ea8af09651f9f08138d57d
ardhanubmp <ardhanu.bmp@gmail.com>
2018-11-01 11:17:15 +0700

    Add wordpress theme starter & grunt task runner

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+   <head>
+      <title>DorebyLeTao</title>
+      <link rel="stylesheet" href="style.css">
+   </head>
+   <body>
+      <h1>Welcome</h1>
+      <p>new theme</p>
+   </body>
+</html>
